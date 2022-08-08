ea9385e6efbb3a65b16f96911b9da67e69af5ba0
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-08 23:59:01 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css';
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1 className="container">Home</h1>
     </div>
   );
---
+++ src/components/navbar.js
@@ -0,0 +1,27 @@
+
+const Navbar = () => {
+    return ( 
+            <div className="navbar">
+                <header>
+                    <div className="header-inner">
+                        <h2 className="logo">LO<span>GO</span></h2>
+                        <i id="bars" className="fas fa-bars bars"></i>
+                        <nav className="nav-menu">
+                        <a href="#" className="nav-link">Home</a>
+                        <a href="#" className="nav-link">About</a>
+                        <a href="#" className="nav-link">Services</a>
+                        <a href="#" className="nav-link">Contact</a>
+                        </nav>
+                    </div>
+                </header>
+                <nav id="mobileMenu" className="mobile-menu">
+                    <a href="#" className="nav-link">Home</a>
+                    <a href="#" className="nav-link">About</a>
+                    <a href="#" className="nav-link">Services</a>
+                    <a href="#" className="nav-link">Contact</a>
+                </nav>
+            </div>
+     );
+}
+ 
+export default Navbar;
