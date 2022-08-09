2ecce7f154fd71f5c315284f1b6f9f42903bc319
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-09 16:15:08 +0200

    write a blog component

---
+++ src/components/CreateBlog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateBlog = () => {
+    return ( 
+        <div className="create-blog">
+            <h2>Write a Blog</h2>
+        </div>
+     );
+}
+ 
+export default CreateBlog;
