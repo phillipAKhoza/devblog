88c0f2aebd7fac1eb5377615fa294b56587e2a28
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-09 16:14:56 +0200

    add write a blog route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/navbar';
 import Home from './components/Home';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import CreateBlog from './components/CreateBlog';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route exact path="/" element={<Home/>} />
+            <Route exact path="/create" element={<CreateBlog/>} />
             {/* <Route path="/about" component={About} />
             <Route path="/blog" component={Blog} />
             <Route path="/contact" component={Contact} /> */}
