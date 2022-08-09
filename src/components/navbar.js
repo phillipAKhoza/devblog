5f850ebc53df23a472947c488937ed72bf4ce4c8
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-09 13:29:05 +0200

    change navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import {Link} from 'react-router-dom'
+
 const Navbar = () => {
-    return ( 
-            <div className="navbar">
-                <header>
-                    <div className="header-inner">
-                        <h2 className="logo">LO<span>GO</span></h2>
-                        <i id="bars" className="fa fa-navicon bars"></i>
-                        <nav className="nav-menu">
-                        <a href="#" className="nav-link">Home</a>
-                        <a href="#" className="nav-link">About</a>
-                        <a href="#" className="nav-link">Services</a>
-                        <a href="#" className="nav-link">Contact</a>
-                        </nav>
-                    </div>
-                </header>
-                <nav id="mobileMenu" className="mobile-menu">
-                    <a href="#" className="nav-link">Home</a>
-                    <a href="#" className="nav-link">About</a>
-                    <a href="#" className="nav-link">Services</a>
-                    <a href="#" className="nav-link">Contact</a>
-                </nav>
-            </div>
-     );
+    const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const Close = () => setClick(false);
+  
+  return (
+    <div>
+     <div className={click ? "main-container" : ""}  onClick={()=>Close()} />
+      <nav className="navbar" onClick={e => e.stopPropagation()}>
+        <div className="nav-container">
+          <Link exact to="/" className="nav-logo">
+            CodeBucks
+            <i className="fa fa-code"></i>
+          </Link>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                exact
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                exact
+                to="/blog"
+                activeClassName="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+              >
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                exact
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+               onClick={click ? handleClick : null}
+              >
+                Contact 
+              </Link>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </ div>
+  );
 }
  
 export default Navbar;
