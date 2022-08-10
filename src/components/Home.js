448c6cfa683bd561d3dd6530d3997ef863e32efe
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-10 19:20:12 +0200

    adding features

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
     const blogs = [ 
@@ -8,7 +8,8 @@ const Home = () => {
         createdAt:"3/14/2010",
         Category:"Research",
         Topic:"Lorem Ipsum",
-        Message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        Message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        Likes:10
         },
         { 
         ID:2,
@@ -16,7 +17,8 @@ const Home = () => {
         Category:"Question",
         Topic:"Lorem Ipsum",
         createdAt:"12/25/0853",
-        Message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        Message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        Likes:20
         },
         { 
         ID:3,
@@ -24,8 +26,15 @@ const Home = () => {
         Topic:"Lorem Ipsum",
         Category:"Discusion",
         createdAt:"1/27/5347",
-        Message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        }]
+        Message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        Likes:20
+        }];
+        const [sortBy,setSortBy] = useState('topics');
+
+        var handleSortBy = (sort)=>{
+            setSortBy(sort);
+        }
+
     return ( 
         <div className="home container">
             <div className="row">
@@ -34,8 +43,10 @@ const Home = () => {
                         {blogs.map((blog) =>(
                             <li key={blog.ID}  className="card">
                                 <h3 className="topic">{blog.Topic}</h3>
-                                <h3 className="authour">Written by {blog.Author}</h3>
-                                <h4 className="category">{blog.Category}</h4>
+                                <div className="header-section">
+                                <h5 className="category">{blog.Category}</h5>
+                                <h5 className="authour">   written by {blog.Author}</h5>
+                                </div>
                                 <p className="created-at">{blog.createdAt}</p>
                                 <p className="blog-message">{blog.Message}</p>
                                 <hr />
@@ -48,8 +59,34 @@ const Home = () => {
                         )}
                     </ul>
                 </div>
-                <div className="col-12 col-xl-4 col-md-4">
-                            Most Popular/ Categories/ Lastest Blogs
+                <div className="col-12 col-xl-4 col-md-4 right-section">
+                    <div className="right-side">
+                        <div className="sorting-buttons">
+                            <button onClick={()=>handleSortBy("topics")}>Topics</button>
+                            <button onClick={()=>handleSortBy("popular")}>Most Popular</button>
+                            {/* <button>Lastest Blogs</button> */}
+                        </div>
+                        <div className="sort-by">
+                            <ul className="sorted-contsiner">
+                                { sortBy === "topics" ? 
+                                    (blogs.map((blog)=>(
+
+                                    <li className="sorted-items">
+                                        <h5 className="category">{blog.Category}</h5>    
+                                    </li>
+                                    )))
+                                    :    
+                                    (blogs.map((blog)=>(
+                                    <li className="sorted-items">
+                                        <h5 className="topic">{blog.Topic}</h5>
+                                        <p className="authour">Written by {blog.Author}</p>
+                                        <p className="category">{blog.Category}</p>
+                                    </li>
+                                    )))
+                                }
+                            </ul>
+                        </div>
+                </div>
                 </div>
             </div>
         </div>
